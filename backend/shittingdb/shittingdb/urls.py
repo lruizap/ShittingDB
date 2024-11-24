e243fbd47add1c29113db5da94d4e0afefebaee3
lruizap <lruizap2502@gmail.com>
2024-11-24 21:52:58 +0100

    finished API

--- backend/shittingdb/shittingdb/urls.py
+++ backend/shittingdb/shittingdb/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Incluye las rutas de la API
+    # Para obtener un token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # Para refrescar un token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
